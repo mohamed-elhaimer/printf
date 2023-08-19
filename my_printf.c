61cdb90cefa05c2c1161e7695ec079a278a1eb5e
oussama-el1 <elhadraoui284@gmail.com>
2023-08-19 14:37:09 -0700

    my_printf.c

---
+++ my_printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* _printf - my printf
+* @format: format for printf
+* Return: an int length of string
+*/
+int _printf(const char *format, ...)
+{va_list args;
+int j = 0, i, len = 0;
+
+convert tab[] = {{"%%", _printfpercentage},
+{"%c", _printfchar}, {"%s", _printfstring}
+};
+va_start(args, format);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
+
+block:
+while (format[j] != '\0')
+{
+for (i = 0; i < 3; i++)
+{
+if (tab[i].ptr_spc[0] == format[j] && tab[i].ptr_spc[1] == format[j + 1])
+{
+len += tab[i].ptr_fun(args);
+j = j + 2;
+
+goto block;
+}
+
+}
+_putchar(format[j]);
+len++;
+j++;
+}
+va_end(args);
+return (len);
+}
+
